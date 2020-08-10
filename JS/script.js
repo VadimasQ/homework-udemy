5e94a59221bd9b68280a31804b378be274e6330f
Miegantis1 <vadimas.kuzminas@gmail.com>
2020-08-10 20:27:08 +0300

    update patikrinimai

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 
 
 
@@ -61,8 +61,10 @@
 // console.log(personalMovieDB);
 //________________________________________________________________________________________________________________________________
 
+//i browseri isvedame popup lentele su klausimu
 const numberOfFilms = +prompt("How many films have you seen already?", '');
 
+//sukuriame objekta
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -71,12 +73,32 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("What is your last move you have wached?", ""),
-      b = +prompt ("Please, rate your last movie from 0 to 10", ""),
-      c = prompt("What is your last move you have wached?", ""),
-      d = +prompt ("Please, rate your last movie from 0 to 10", "");
+//parasome cikla pasikartojanti 2 kartus
+for (let i = 0; i < 2; i++) {
+    const a = prompt("What is your last move you have wached?", ""),
+          b = +prompt ("Please, rate your last movie from 0 to 10", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    //aprasome salygas kur: null tai atsauktas langas a!=null, && = ir, patikrinima ar nera tusciu ivedimu a!="" ir teksto ilgis nedidesnis nei 50 simboliu (a.lenght < 50)
+          if (a != null && b != null && a != "" && b !="" && a.length < 50) {
+            //duomenys irasome i objekta personalMovieDB movies
+          personalMovieDB.movies[a] = b;
+          console.log("Done");
+    } else {
+        // jei nors viena if salyga netenkima rasome, kad klaida ir maziname i, kad ciklas pasikartotu
+        console.log("error");
+        i--;
+    }
+
+}
+
+if (personalMovieDB.count <= 10) {
+    console.log("You are rookie!");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log("You are average movie man");
+} else if (personalMovieDB.count >= 30) {
+    console.log("You are advenced rocket racoon");}
+    else {console.log("Don't play with me!");}
+
 
 console.log(personalMovieDB);
+
